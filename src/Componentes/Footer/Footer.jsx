e0bead413853d6f118c099e4f70d797e729979d7
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-06-15 12:09:26 -0300

    Adicionando footer

--- src/Componentes/Footer/Footer.jsx
+++ src/Componentes/Footer/Footer.jsx
@@ -18,6 +18,20 @@ function Footer () {
                     <div className="footer-logo-col">
                         <img src={ Logo }/>
                         <p className="gray-1-color">A escola que prepara você para as profissões em alta no mercado de trabalho</p>
+                        <div className="d-flex social-links">
+                            <a href="https://www.facebook.com/profile.php?id=61573785916713&locale=pt_BR" target="_blank">
+                                <img src={ iconFacebook } alt="" />
+                            </a>
+                            <a href="https://x.com/_Lzakinarchi" target="_blank">
+                                <img src={ iconTwitter } alt="" />
+                            </a>
+                            <a href="https://www.linkedin.com/in/luis-sene/" target="_blank">
+                                <img src={ iconLinkedin } alt="" />
+                            </a>                            
+                            <a href="https://www.instagram.com/thrsh.br/?hl=pt-br" target="_blank">
+                                <img src={ iconInstagram } alt="" />
+                            </a>
+                        </div>
                     </div>
                 </div>
 
