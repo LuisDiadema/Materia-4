74b1e3ecdf46340bfa94c2292b195f3b097cee29
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-06-16 13:12:35 -0300

    Adicionando botões na pagina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Header from "../Componentes/Header/Header"
+import Button from "../Componentes/Button/Button"
 import Footer from "../Componentes/Footer/Footer"
 
 function Home () {
@@ -7,6 +8,7 @@ function Home () {
         <Header />
         <h1>Home</h1>
         <p>Subtitulo</p>
+        <Button buttonStyle="primary">Luis Gostoso</Button>
         <Footer />
         </>
     )
