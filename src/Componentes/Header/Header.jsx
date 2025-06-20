41e39e289841a29dd7da90e15aa3cdde788e21e6
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-06-20 12:43:02 -0300

    Menu mobile adicionado

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -11,18 +11,26 @@ import Button from '../Button/Button'
 function Header () {
     const [isOpen, setIsOpen] = useState(false)
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <header>
             <div className="container">
                 <div className="al-center d-flex jc-space-between">
                     <Link to="/"><img src={ Logo }/></Link>
                     <div className="mobile-menu">
-                        <Button buttonStyle="secondary" onClick={() => setIsOpen(!isOpen)}>
+                        <Button buttonStyle="secondary" onClick={toggleMenu}>
                             Menu
                         </Button>
                     </div>
                     <nav className={`${isOpen ? 'open' : ''}`}>
-                        <Button buttonStyle="unstyled" className="close-btn">X</Button>
+                        <Button 
+                            buttonStyle="unstyled" 
+                            className="mobile-menu close-btn" 
+                            onClick={toggleMenu}>X</Button>
+                            
                         <ul className="d-flex">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="About">About</Link></li>
