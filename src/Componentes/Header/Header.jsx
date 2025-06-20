9782db6be2440988ea44c4620f51da46b91275ca
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-06-20 12:36:58 -0300

    Adicionando menu mobile

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -1,23 +1,35 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 // Assets
 import './Header.css'
 import Logo from '../../assets/dnc-logo.svg'
 
+// Components
+import Button from '../Button/Button'
+
 function Header () {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <header>
             <div className="container">
                 <div className="al-center d-flex jc-space-between">
-                    <Link to="/"><img src={Logo}/></Link>
-                                    <nav>
-                    <ul className="d-flex">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="About">About</Link></li>
-                        <li><Link to="Projects">Projects</Link></li>
-                        <li><Link to="Contact">Contact</Link></li>
-                    </ul>
-                </nav>
+                    <Link to="/"><img src={ Logo }/></Link>
+                    <div className="mobile-menu">
+                        <Button buttonStyle="secondary" onClick={() => setIsOpen(!isOpen)}>
+                            Menu
+                        </Button>
+                    </div>
+                    <nav className={`${isOpen ? 'open' : ''}`}>
+                        <Button buttonStyle="unstyled" className="close-btn">X</Button>
+                        <ul className="d-flex">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="About">About</Link></li>
+                            <li><Link to="Projects">Projects</Link></li>
+                            <li><Link to="Contact">Contact</Link></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </header>
