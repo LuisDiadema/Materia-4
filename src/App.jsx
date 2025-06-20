53b6c3c73e4589dbf35add3a79046005f17fac2b
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-06-20 13:41:18 -0300

    Adicionando funcionalidade de levar o usuario ao topo assim que trocar de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,18 @@ import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 
+// Utils
+import ScrollToTop from './Utils/ScrollTop'
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Home />} ></Route>
-        <Route path="/about" element={<About />} ></Route>
-        <Route path="/projects" element={<Projects />} ></Route>
-        <Route path="/contact" element={<Contact />} ></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </Router>
   )
