adf598b018b95f500351296b7229f5f9b50dd883
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-06-23 11:16:24 -0300

    Inserindo API

--- src/Componentes/ProjectsList/ProjectsList.jsx
+++ src/Componentes/ProjectsList/ProjectsList.jsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from 'react'
 import './ProjectsList.css'
 
 // Assets
-
 import likeFilled from '../../assets/like-filled.svg'
 import like from '../../assets/like.svg'
 
+// Utils
+import { getApiData } from '../../services/apiServices'
+
 function ProjectsList () {
+    const [projects, setProjects] = useState()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const projectsResponse = await getApiData('projects')
+                setProjects(projectsResponse)
+            } catch {
+                setProjects([])
+            }
+        }
+
+        fetchData()
+    }, [])
+
     return (
         <div className="projects-section">
             <div className="projects-hero">
@@ -13,6 +31,11 @@ function ProjectsList () {
                 <p>Follow Our Projects</p>
             </div>
             <div className="projects-grid">
+                {
+                    projects.map((project) => (
+                        <div key={project.id}></div>
+                    ))
+                }
                 <div className="project-card jc-center al-center fd-center">
                     <div className="thumb tertiary-background"></div>
                     <h3>Luisinho Diadema</h3>
