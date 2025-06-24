d20bd0190a53a5c61d3bfc855a7b3ba3fe5b156b
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-06-24 10:38:52 -0300

    Adicionando formlario e funcionalidade de submit a pagina de contact

--- src/Componentes/ContactForm/ContactForm.jsx
+++ src/Componentes/ContactForm/ContactForm.jsx
@@ -5,6 +5,42 @@ import './ContactForm.css'
 import Button from '../Button/Button'
 
 function ContactForm () {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+    const [isFormValid, setIsFormValid] = useState(false) 
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (isFormValid) {
+            null
+        } 
+    }
+
+    useEffect(() => {
+        const isValidEmail = (email) => {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+            return emailRegex.test(email)
+        }
+
+        const isValid = formData.name.trim() && 
+        formData.email.trim() &&
+        isValidEmail(formData.email) &&
+        formData.message.trim()
+
+        setIsFormValid(isValid)
+    }, [formData])
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
     return (
         <div>
             <div className="contact-form d-flex fd-column al-center">
@@ -16,12 +52,14 @@ function ContactForm () {
                             type="text"
                             id="name"
                             placeholder="Name *"
+                            onChange={handleChange}
                         />
                         <input 
                             className="form-input"
                             type="email"
                             id="email"
                             placeholder="E-mail *"
+                            onChange={handleChange}
                         />
                     </div>
                     <div className="d-flex form-group">
@@ -30,11 +68,12 @@ function ContactForm () {
                             id="message"
                             name="message" 
                             placeholder="Mensagem *"
+                            onChange={handleChange}
                             rows="4"
                         ></textarea>
                     </div>
                     <div className="al-center d-flex jc-end form-group">
-                        <Button type="submit" buttonStyle="secondary">
+                        <Button type="submit" buttonStyle="secondary" disabled={!isFormValid}>
                             Enviar
                         </Button>
                     </div>
